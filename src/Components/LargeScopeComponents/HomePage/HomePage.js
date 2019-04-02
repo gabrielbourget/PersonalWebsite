a25d3b25ab41788f4612eb9b606f22f69015fbdb
Gabriel Bourget <gabrielbourget@gmail.com>
2019-04-01 23:04:02 -0400

    matched right padding on nav bar with left padding.

--- src/Components/LargeScopeComponents/HomePage/HomePage.js
+++ src/Components/LargeScopeComponents/HomePage/HomePage.js
@@ -22,90 +22,9 @@ class HomePage extends React.Component {
 		const keyPlaylistsClasses = ClassNames(styles.keyPlaylists, themeClass);
 
 		return (
-			<React.Fragment>
 				<FromTheTopCradle>
 					<Jumbotron/>
-				</FromTheTopCradle>
-				<div className={ bottomContentClasses }>
-					<h2 className={ styles.introToWaveBlurb }>
-						Out of the new, distributed reality of social networks and streaming platforms, a global 
-						community of artists has gathered around a type of music collectively known as wave. Read more about this 
-						community, and the art being created and shared within it below. 
-					</h2>
-
-					<div style={ {display:'grid', width: '100%', placeContent: 'center'} }>
-						<Link to='/documentation/97df66de-33fb-48cd-9543-0c698821d5d8'>
-							<FilledButton text='READ FURTHER' shape='square' onClick={ () => {} }/>
-						</Link>
-					</div>
-
-					<HorizontalDivider height='4px'/>
-				</div>	
-				<div className={ keyPlaylistsCradleClasses }>
-					<div className={ keyPlaylistsClasses }>
-						<h2>
-							Click through to these spotify playlists to get an introduction to some of the sounds of the scene.
-						</h2>
-						<ul style={ {paddingLeft: '20px', display: 'grid', alignItems: 'center', justifyItems: 'start' } }>
-							<li>
-								<p>
-									<a 
-										href="https://open.spotify.com/user/liquidritual/playlist/57GAUprXFP9XIZaupQzIsS?si=vhEAliiAT0K16N1KRNISlw"
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										Wave Essentials (Liquid Ritual)
-									</a>
-								</p>
-							</li>
-							<li>
-								<p>
-									<a 
-										href="https://open.spotify.com/album/2MeQtgz5OzKfkOoMqHlhFr?si=5hP5A3jkTj6s6F13RzIAog"
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										Liquid Ritual Volume 1 
-									</a>
-								</p>
-							</li>	
-							<li>
-								<p>
-									<a 
-										href="https://open.spotify.com/album/3Godq9AabVjhEVCuJPFGIF?si=JzD7OiiWRkO53ZhzOGWH7Q"
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										Vibe Digital Volume 1 
-									</a>
-								</p>
-							</li>	
-							<li>
-								<p>
-									<a 
-										href="https://open.spotify.com/user/soundofshade/playlist/7lmO3tR8k5Rf0xUNPBUfV3?si=cfc_I885RICy07Adtu4y4A"
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										Shxde Selects
-									</a>
-								</p>
-							</li>	
-							<li>
-								<p>
-									<a 
-										href="https://open.spotify.com/playlist/6inZMdZy0O3v1oVm2CicAi?si=Mgnxa7yaQhymj-6vWzEPvQ"
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										Wave | Future Garage | Dark Trap (maintained by c o l d k i s s)
-									</a>
-								</p>
-							</li>	
-						</ul>							
-					</div>		
-				</div>				
-			</React.Fragment>			
+				</FromTheTopCradle>			
 		);
 	}
 }
